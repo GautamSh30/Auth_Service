b05c0a75ad0016a0b394f88bd2f0a439ba7908d1
Gautam Sharma <gautam2586272@gmail.com>
2024-05-10 22:29:27 +0530

    added getuserbyid in repo

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ const app = express();
 const { PORT } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 
+// const UserRepository = require("./repository/user-repository");
+
 const prepareAndStartServer = () => {
   app.use(bodyParser.json());
 
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.use("/api", apiRoutes);
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
     console.log(`Sever Started on ${PORT}`);
+    // const repo = new UserRepository();
+    // const response = await repo.getById(1);
+    // console.log(response);
   });
 };
 
